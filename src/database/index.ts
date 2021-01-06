8e36899afa5d57291bac33826872dec7308cadc6
Guizi <ricardo.guizi@ewally.com.br>
2021-01-06 14:52:41 -0300

    connection config fixed

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,7 +2,8 @@ import { createConnection } from 'typeorm'
 
 createConnection({
   type: 'postgres',
-  url: process.env.DATABASE_URL
+  url: process.env.DATABASE_URL,
+  ssl: true
 })
 
 // createConnection(process.env.DATABASE_URL ? 'remote' : 'local')
