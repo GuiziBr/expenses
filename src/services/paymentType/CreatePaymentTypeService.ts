bfc50c5fd5402554cfff57ceac721e2d8bc23a68
Ricardo Guizi <guizi189@gmail.com>
2022-06-15 23:31:16 -0700

    fixing create bank, category, paymentType and store using description from an already deleted record (#28)

--- src/services/paymentType/CreatePaymentTypeService.ts
+++ src/services/paymentType/CreatePaymentTypeService.ts
@@ -5,17 +5,25 @@ import PaymentType from '../../models/PaymentType'
 import { IPaymentType } from '../../domains/paymentType'
 
 class CreatePaymentTypeService {
-  public async execute(description: string): Promise<Omit<IPaymentType, 'updated_at'>> {
-    const paymentTypeRepository = getRepository(PaymentType)
-    const paymentTypeExists = await paymentTypeRepository.findOne({ where: { description }})
-    if (paymentTypeExists) throw new AppError(constants.errorMessages.existingPaymentType)
-    const paymentType = paymentTypeRepository.create({ description })
-    await paymentTypeRepository.save(paymentType)
+  private assemblePaymentTypeResponse(paymentType: IPaymentType) {
     return {
       id: paymentType.id,
       description: paymentType.description,
       created_at: paymentType.created_at
     }
+  }
+
+  public async execute(description: string): Promise<Omit<IPaymentType, 'updated_at'>> {
+    const paymentTypeRepository = getRepository(PaymentType)
+    const existingPaymentType = await paymentTypeRepository.findOne({ where: { description }, withDeleted: true })
+    if (existingPaymentType) {
+      if (!existingPaymentType.deleted_at) throw new AppError(constants.errorMessages.existingPaymentType)
+      else await paymentTypeRepository.restore(existingPaymentType.id)
+      return this.assemblePaymentTypeResponse(existingPaymentType)
+    }
+    const paymentType = paymentTypeRepository.create({ description })
+    await paymentTypeRepository.save(paymentType)
+    return this.assemblePaymentTypeResponse(paymentType)
   }
 }
 
