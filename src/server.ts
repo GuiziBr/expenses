ef0b3290e1202f7a57b4b38f6f6d0956cc19430d
Ricardo Guizi <guizi189@gmail.com>
2022-09-07 22:33:49 -0700

    Aws migration (#48)
    * Update actions
    
    * Update cors
    
    * Update cors
    
    * Add redis to set rate limiter
    
    * Update typescript
    
    * Add Sentry monitoring
    
    * Update rate limit
    
    * Remove redis

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
+import * as Sentry from '@sentry/node'
+import * as Tracing from '@sentry/tracing'
 import cors from 'cors'
 import { CronJob } from 'cron'
 import { isLastDayOfMonth } from 'date-fns'
 import dotenv from 'dotenv'
 import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
-import 'reflect-metadata'
-import swaggerUi from 'swagger-ui-express'
 import { OpenApiValidator } from 'express-openapi-validator'
 import { OpenAPIV3 } from 'express-openapi-validator/dist/framework/types'
+import 'reflect-metadata'
+import swaggerUi from 'swagger-ui-express'
 import apiSchema from './api.schema.json'
 import uploadConfig from './config/upload'
 import constants from './constants'
@@ -19,13 +21,50 @@ import ReportService from './services/report/ReportService'
 dotenv.config()
 const app = express()
 
+if (process.env.LOCAL === 'false') {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+      // enable Express.js middleware tracing
+      new Tracing.Integrations.Express({ app })
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0
+  })
+}
+
+if (process.env.LOCAL === 'false') {
+  app.use(Sentry.Handlers.requestHandler())
+  app.use(Sentry.Handlers.tracingHandler())
+}
+
 app.use(cors({
   origin: constants.corsOrigins,
   exposedHeaders: constants.headerTypes.totalCount
 }))
+
 app.use(express.json())
 app.use('/files', express.static(uploadConfig.directory))
 app.use(routes)
+
+if (process.env.LOCAL === 'false') {
+  app.use(
+    Sentry.Handlers.errorHandler({
+      shouldHandleError(error) {
+        if (error.statusCode === 429 || error.statusCode === 500) {
+          return true
+        }
+        return false
+      }
+    })
+  )
+}
+
 app.use((err: Error, _request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) return response.status(err.statusCode).json({ status: 'error', message: err.message })
   console.error(err)
